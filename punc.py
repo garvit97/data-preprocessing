35efd662b9769015c6fb1e01246782196d95f490
garvit97 <guptagarvit1997@gmail.com>
2017-06-30 18:50:31 +0900

    Add files via upload

--- punc.py
+++ punc.py
@@ -6,6 +6,7 @@ sys.setdefaultencoding('utf8')
 
 filename=sys.argv[1]#raw_input("Enter filename: ")
 outFileName=sys.argv[2]
+
 f = open(str(filename), 'r')
 f1=open("temp",'w')
 
@@ -35,7 +36,30 @@ for line in f2:
 		f3.write(" ")
 
 f2.close()
+f3.close()		
+
+wordList=[]
+puncList=[[",","?","!",".","..","...","....",":",";","-","--","\"","\'","(",")","#"],[",COMMA","?QUESTIONMARK","!EXCLAMATIONMARK",".PERIOD","..DOUBLEDOTS","...TRIPLEDOTS","....FOURDOTS",":COLON",";SEMICOLON","-DASH","--DOUBLEDASH","\"DOUBLEQUOTES","\'SINGLEQUOTE","(BRACKETOPEN",")BRACKETCLOSE","#HASHTAG"],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+f4=open(str(outFileName)+"1",'w')
+f3=open(str(outFileName),'r')
+for line in f3:
+	for word in line.split():
+		for i in range(len(puncList[0])):
+			if puncList[0][i]==word:
+				word=puncList[1][i]
+				puncList[2][i]+=1
+		f4.write(str(word))
+		f4.write(" ")
+		#wordList.append(word)
+f5=open(str(outFileName)+"stats",'w')
+for i in range(len(puncList[0])):
+	f5.write(str(puncList[1][i])+"\t"+str(puncList[2][i])+"\n")
+
 f3.close()
+f4.close()
+f3.close()
+
 
 
 
